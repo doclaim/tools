28e0e4ecb1d55a7fef6783988c28f75754c6d203
doclaim <540587562@qq.com>
2014-11-17 16:16:28 +0800

    fff

--- centos_desk_vnc/vnc_install.sh
+++ centos_desk_vnc/vnc_install.sh
@@ -3,12 +3,12 @@
 sed -i 's/id:[0-9]/id:5/g' /etc/inittab
 tail -n 1 /etc/inittab
 
-echo "--------begin stall vnc-----------"
-yum install -y *vnc*
-[ ! -e /root/.vnc/xstartup ] && echo "vnc server not exists"
+echo "--------begin start vnc-----------"
+yum install -y "*vnc*"
+[ ! -e /root/.vnc/xstartup ] && echo "vnc server not exists" || {
 rm -rf /root/.vnc/xstartup && cp xstartup /root/.vnc/
-
 vncserver -kill :1
 vncserver :1
-
 ps -aux|grep vnc
+}
+
